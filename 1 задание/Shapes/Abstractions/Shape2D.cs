526bb302c4ed120d51feb8bc0758c2b477510bae
Ibragim Kurbanov <sasha.dragunov1@mail.ru>
2023-11-14 20:43:00 +0300

    Выполненное тестовое задание

---
+++ 1 задание/Shapes/Abstractions/Shape2D.cs
@@ -0,0 +1,18 @@
+﻿using Shapes.Interfaces;
+using Shapes.Structures;
+
+namespace Shapes.Abstractions
+{
+    public abstract class Shape2D : Shape, IShape2D
+    {
+        public Point2D Center { get; protected set; }
+        public double Perimeter { get; protected set; }
+
+        protected Shape2D(Point2D center) => Center = center;
+
+        public override string ToString()
+            => $"Фигура: {GetType().Name}, Периметр: {Perimeter}, Площадь: {Area}.";
+
+        protected abstract void CalculatePerimeter();
+    }
+}
